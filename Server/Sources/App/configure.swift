9a7b74be96ba928b799246fbbd1f502460257e2c
Natan Rolnik <natanrolnik@gmail.com>
2022-04-28 01:15:38 +0300

    Allow CORS in server

--- Server/Sources/App/configure.swift
+++ Server/Sources/App/configure.swift
@@ -9,6 +9,14 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    // cors middleware should come before default error middleware using `at: .beginning`
+    let config = CORSMiddleware.Configuration(
+        allowedOrigin: .originBased,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .init("X-Nutmeg-User-Id")]
+    )
+    app.middleware.use(CORSMiddleware(configuration: config), at: .beginning)
+
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
