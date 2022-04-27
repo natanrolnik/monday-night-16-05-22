a968a96d1e333bbd7e1220c5831de994a2c01fb0
Natan Rolnik <natanrolnik@gmail.com>
2022-04-28 01:16:14 +0300

    Implement raffle and registering nutmegs

--- Web/src/components/NutmegStars.vue
+++ Web/src/components/NutmegStars.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="NutmegStars">
+    <h1>Welcome to Nutmeg Stars!</h1>
+
     <div v-if="userId == null">
-      <h1>Welcome to Nutmeg Stars!</h1>
       <h2>Add your name to play with us!</h2>
       <h3>In the end, we'll raffle a book</h3>
       <input v-model.trim="inputUserName" type="text" placeholder="Your name and last name"/>
       <button v-on:click="this.createUser()" class="btn btn-primary">Become a Star</button>
-      </div>
-    <div v-else>
-      User exists!
     </div>
+
+    <div v-else>
+      <div v-if="isRaffleIdle">
+        <h2>Hello, {{ userName }}</h2>
+        <button v-on:click="this.registerNutmeg()" class="btn btn-primary">Nutmeg!</button>
+        <p>
+        Nutmegs so far: {{ nutmegsCount }}
+        </p>
+      </div>
+      <div v-else>
+        <h2>Book Raffle 📚</h2>
+        <div v-if="candidateUserName != null">
+          <h3>{{ candidateUserName }}</h3>
+        </div>
+        <div v-if="winnerUser != null">
+          <div v-if="winnerUser.id == this.userId">
+            Congrats, {{ winnerUser.name }}! You just won a book!
+          </div>
+          <div v-else>
+            The winner is {{ winnerUser.name }}. Thanks for joining anyway!
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -18,6 +41,11 @@ const axios = require('axios')
 
 export default {
   name: 'NutmegStars',
+  mounted () {
+    if (this.userId != null) {
+      this.openRaffleSocket()
+    }
+  },
   data () {
     return {
       get userId () {
@@ -32,7 +60,12 @@ export default {
       set userName (value) {
         localStorage.setItem('nutmeg-stars-user-name', value)
       },
-      inputUserName: null
+      inputUserName: null,
+      nutmegsCount: 0,
+      isRaffleIdle: true,
+      candidateUserName: null,
+      winnerUser: null,
+      openedSocket: false
     }
   },
   methods: {
@@ -41,10 +74,55 @@ export default {
         const res = await axios.post('http://localhost:8090/users/new', { name: this.$data.inputUserName })
         this.userId = res.data.user.id
         this.userName = res.data.user.name
+        this.openRaffleSocket()
       } catch (error) {
         alert("Creating user failed. Make sure you're using a unique name.")
       }
-      // axios.get('http://localhost:8090/hello')
+    },
+    async registerNutmeg () {
+      try {
+        const config = {
+          headers: {
+            'X-Nutmeg-User-Id': this.$data.userId
+          }
+        }
+        const res = await axios.put('http://localhost:8090/nutmegs/increment', {}, config)
+        this.nutmegsCount = res.data.count
+      } catch (error) {
+        alert('Something went wrong')
+      }
+    },
+    openRaffleSocket () {
+      if (this.openedSocket) {
+        return
+      }
+
+      this.openedSocket = true
+      const connection = new WebSocket('ws://localhost:8090/raffle/live')
+
+      const self = this
+      connection.onmessage = function (event) {
+        const status = JSON.parse(event.data)
+
+        if (self.winnerUser != null && self.winnerUser.id === self.userId) {
+          return
+        }
+
+        if (status.idle != null) {
+          self.isRaffleIdle = true
+          self.winnerUser = null
+          self.candidateUserName = null
+        } else {
+          self.isRaffleIdle = false
+
+          if (status.running != null) {
+            self.candidateUserName = status.running.candidate
+          } else if (status.finished != null) {
+            self.candidateUserName = null
+            self.winnerUser = status.finished.winner
+          }
+        }
+      }
     }
   },
   props: {
